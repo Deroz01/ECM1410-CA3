102ad46328fa37720109b9d98a8dddfe12ccbc58
Deroz01 <75436023+Deroz01@users.noreply.github.com>
2021-04-21 16:25:46 +0100

    Update Endorsement.java

--- socialmedia/Endorsement.java
+++ socialmedia/Endorsement.java
@@ -1,18 +1,24 @@
+package socialmedia;
 
-public class Endorsement {
- private String message;
- 
- private String handle;
- 
- public public Endorsement(String message, String handle) {
-	// TODO Auto-generated constructor stub
-	this.message = message;
-	this.handle = handle;
- }
- 
- @Override
+import java.util.ArrayList;
+
+public class Endorsement extends Post {
+
+	public Endorsement(String handle, String message) {
+		super(handle, message);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "EP@" + handle + ": " + message;
+		return "EP@" + getHandle() + ": "+ getMessage();
+	}
+	public static void main(String[] args) {
+		Post endorsement = new Endorsement("handle", "message");
+		System.out.println(endorsement);
+		
+		ArrayList<Post> posts = new ArrayList<>();
+		
+		posts.add(endorsement);
 	}
 }
