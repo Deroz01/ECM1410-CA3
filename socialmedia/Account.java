512cc34852f415095faa814af2e68d2c7e8afba9
tmacksf <33499837+tmacksf@users.noreply.github.com>
2021-04-29 15:08:10 +0100

    Update Account.java

--- socialmedia/Account.java
+++ socialmedia/Account.java
@@ -3,7 +3,7 @@ package socialmedia;
 import java.io.Serializable;
 
 public class Account implements Serializable{
-	private int id;
+    private int id;
     private String handle;
     private String description;
     private int postCount;
@@ -11,55 +11,110 @@ public class Account implements Serializable{
 
     private static int idNumber = 0;
 
+    /**
+     * Method to get the user's handle
+     * @return A String of the user's handle
+     */
     public String getHandle() {
         return handle;
     }
+
+    /**
+     * Method to get the user's description
+     * @return A String of the user's description
+     */
     public String getDescription() {
         return description;
     }
+
+    /**
+     * Method to get the user's ID
+     * @return A String of the user's ID
+     */
     public int getId() {
         return id;
     }
+
+    /**
+     * Method to get the number of user's posts
+     * @return An int of how many posts the user has
+     */
     public int getPostCount() {
-		return postCount;
-	}
+        return postCount;
+    }
+
+    /**
+     * Method to add a post to the user's post count
+     */
     public void increasePostCount() {
-    	postCount++;
+        postCount++;
     }
+
+    /**
+     *  Method to get the number of endorsements the user has received
+     * @return An int of how many endorsements the user has received
+     */
     public int getEndorseCount() {
-		return endorseCount;
-	}
-    public void increaseEndorseCount() {
-    	endorseCount++;
+        return endorseCount;
     }
+
+    /**
+     * Method to increase the number of endorsements the user has recieved
+     */
+    public void increaseEndorseCount() {
+        endorseCount++;
+    }
+
+    /**
+     * Method to set the user's handle
+     * @param handle the handle that will be set
+     */
     public void setHandle(String handle) {
         this.handle = handle;
     }
+
+    /**
+     * Method to set the user's description
+     * @param description the description that will be set
+     */
     public void setDescription(String description) {
         this.description = description;
     }
+
+    /**
+     * Method to set the id number of the account to 0
+     */
     public static void resetIdCounter() {
-    	idNumber = 0;
+        idNumber = 0;
     }
 
+    /**
+     * Constructor for the account
+     * @param handle The String that will be the user's account handle
+     */
     public Account (String handle) {
         this.handle = handle;
         this.id = idNumber;
         this.description = "";
         idNumber++;
     }
+
+    /**
+     * Constructor for the account
+     * @param handle The String that will be the user's account handle
+     * @param description The String that will be the user's account description
+     */
     public Account (String handle, String description) {
         this(handle);
         this.description = description;
     }
-	@Override
-	public String toString() {
-		return "ID: " + id + "\nHandle: " + handle + "\nDescription: " + description + "\nPost count: " + postCount + "\nEndorse count: " + endorseCount;
-	}
-	public static void main(String[] args) {
-		Account user1 = new Account("user1");
-		System.out.println(user1);
-	}
-	
-    
+
+    /**
+     * Method to output the correct string format for an account
+     * @return The String that describes the the account
+     */
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nHandle: " + handle + "\nDescription: " + description + "\nPost count: " + postCount + "\nEndorse count: " + endorseCount;
+    }
 }
